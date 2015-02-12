0fa679e73db61764873768b93321044f7f2a4f04
onaliugo <hello@onali-ugo.fr>
2015-02-12 17:17:54 +0100

    Generate a CSS file via template #1

--- example.js
+++ example.js
@@ -3,7 +3,8 @@ var SVGSpriter = require('./lib/index');
 var Spriter = new SVGSpriter({
 	file: 'icons.sketch',
 	dist: 'assets/',
-	padding: 20
+	padding: 50,
+	cssTemplatePath: './template.css'
 })
 
 Spriter.generate();
